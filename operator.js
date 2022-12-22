bb8802efbde623bc9e3dfdf62b2b26dab732402d
heojane <pepupx2@naver.com>
2022-12-23 00:06:31 +0900

    operators

---
+++ operator.js
@@ -0,0 +1,26 @@
+// 1. string concatenation
+console.log('my' + ' cat'); //my cat
+console.log('1'+2); //12
+console.log(`string literals: 1+2=${1+2}`); //string literals: 1+2=3
+
+// 2. numeric operators
+console.log(1+1); //2
+console.log(1-1); //0
+console.log(1/1); //1
+console.log(1*1); //1
+console.log(5%2); //1
+console.log(2**3); //8
+
+// 3. increment and decrement operators
+let counter = 2;
+const preIncrement = ++count;
+//counter = counter + 1;
+//preIncrement = counter;
+console.log(`preIncrement: ${preIncrement}, counter: ${counter}`); //preIncrement: 3, counter: 3
+
+const postIncrement = count++;
+//postIncrement = counter;
+//counter = counter + 1;
+console.log(`postIncrement: ${postIncrement}, counter: ${counter}`); //postIncrement: 3, counter: 4
+
+
